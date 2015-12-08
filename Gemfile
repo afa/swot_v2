a78bd7973aad886c1be68be987c005e8347ee067
oleg <afa@cnwork.techranch.local>
2015-12-08 18:54:39 +0300

    current

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
 # A sample Gemfile
 source "https://rubygems.org"
 gem 'hiredis'
+gem 'ohm'
 gem 'celluloid', require: 'celluloid/current'
+gem 'celluloid-redis'
+gem 'celluloid-io'
 gem 'sinatra'
 gem 'thin'
 gem 'racksh'
--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,15 @@
 require 'sinatra/base'
+require 'timers'
 class Game < Sinatra::Base
   include Celluloid
+  timers = Timers::Group.new
+  configure do
+    enable :logging 
+  end
+  get '/' do
+    'ok'
+  end
+  every_five_seconds = timers.every(5) { puts "Another 5 seconds" }
+
+  # loop { timers.wait }
 end
