33cef01a377c17b0f4c8a111d18de72fdc02c4d1
oleg <afa@cnwork.techranch.local>
2015-12-16 18:55:35 +0300

    current

--- lib/control.rb
+++ lib/control.rb
@@ -1,25 +1,35 @@
 class Control
   include Celluloid
-  CONTROL_CHANNEL = 'swot/control'
+  include Celluloid::Internals::Logger
+  include Celluloid::IO
+  CONTROL_CHANNEL = '/swot/control'
+  finalizer :finalizer
 
   attr_accessor :control_channel
-  def initialize
+  def initialize params = {}
+    info "starting control"
     @redis = ::Redis.new(driver: :celluloid)
-    
+    @channel_name = params[:channel] || CONTROL_CHANNEL
+    async.run
+    info "start control"
   end
 
   def run
-    self.control_channel = @redis.subscribe(CONTROL_CHANNEL) do |on|
+    self.control_channel = @redis.subscribe(@channel_name) do |on|
       on.message do |ch, msg|
-        p msg
+        info "#{ch.inspect} :: #{msg.inspect}"
       end
-      on.subscribe do
-        p 'sub'
+      on.subscribe do |ch, subs|
+        info "sub #{ch.inspect} -- #{subs.inspect}"
       end
       on.unsubscribe do
-        p 'un'
+        info 'un'
       end
     end
-    p 'ex ctl'
+  end
+  def finalizer
+    info "stop control"
+    # self.control_channel.publish "done"
+    @redis.unsubscribe '/swot/control'
   end
 end
