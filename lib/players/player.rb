e3a7cbf7f781b733d1840a0fb34d5a476fc37b1e
oleg <afa@cnwork.techranch.local>
2015-12-24 19:35:12 +0300

    try

--- lib/players/player.rb
+++ lib/players/player.rb
@@ -1,19 +1,25 @@
 class Player
   include Celluloid
   include Celluloid::IO
+  include Celluloid::Internals::Logger
 
   finalizer :finalizer
 
   attr_accessor :name, :email, :channel, :game_uuid, :uuid
 
   def initialize params = {}
-          Store::Player.create(params.merge(game_uuid: @uuid))
+    if params[:uuid]
+    else
+    info "player #{params.inspect} started"
+    Store::Player.create(params.merge(game_uuid: @uuid))
+    end
   end
 
   def run
   end
 
   def finalizer
+    info 
   end
 
 end
