728edfb37106c544bec9157404d079365014477a
oleg <afa@cnwork.techranch.local>
2016-01-20 19:47:45 +0300

    current

--- lib/control.rb
+++ lib/control.rb
@@ -1,3 +1,4 @@
+require 'march_hare'
 class Control
   include Celluloid
   include Celluloid::Internals::Logger
@@ -8,41 +9,44 @@ class Control
   attr_accessor :control_channel
   def initialize params = {}
     info "starting control"
-    @sub = ::Redis.new(driver: :celluloid)
-    @redis = ::Redis.new(driver: :celluloid)
+    @conn = MarchHare.connect
+    @ch = @conn.create_channel
+    # @sub = ::Redis.new(driver: :celluloid)
+    # @redis = ::Redis.new(driver: :celluloid)
     @channel_name = params[:channel] || CONTROL_CHANNEL
     async.run
     info "start control"
   end
 
   def run
+    
     # @redis.subscribe(@channel_name, '/game/*', '/player/*') do |on|
-    @sub.psubscribe('/*/*') do |on|
-      on.pmessage do |pat, ch, msg|
-        info "#{ch.inspect} :: #{msg.inspect}"
-        sel = begin
-                MultiJson.load(msg)
-              rescue Exception => e
-                {error: e.message}
-              end
+    # @sub.psubscribe('/*/*') do |on|
+    #   on.pmessage do |pat, ch, msg|
+    #     info "#{ch.inspect} :: #{msg.inspect}"
+    #     sel = begin
+    #             MultiJson.load(msg)
+    #           rescue Exception => e
+    #             {error: e.message}
+    #           end
 
-        info sel.inspect
-        unless sel[:error]
-          klass = ::Message.parse(ch, sel)
-          info klass
-          klass.new(ch, sel).process if klass
-        end
-      end
-      on.psubscribe do |ch, subs|
-        # self.control_channel = on
-        info "sub #{ch.inspect} -- #{subs.inspect}"
-        info on.inspect
-      end
-      on.punsubscribe do
-        info 'un'
-        async.stop
-      end
-    end
+    #     info sel.inspect
+    #     unless sel[:error]
+    #       klass = ::Message.parse(ch, sel)
+    #       info klass
+    #       klass.new(ch, sel).process if klass
+    #     end
+    #   end
+    #   on.psubscribe do |ch, subs|
+    #     # self.control_channel = on
+    #     info "sub #{ch.inspect} -- #{subs.inspect}"
+    #     info on.inspect
+    #   end
+    #   on.punsubscribe do
+    #     info 'un'
+    #     async.stop
+    #   end
+    # end
   end
 
   def stop
