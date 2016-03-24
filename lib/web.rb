26268ee3a1544169ad4eca9ce66fc06c49e26be3
oleg <afa@cnwork.techranch.local>
2016-03-24 18:59:11 +0300

    snd

--- lib/web.rb
+++ lib/web.rb
@@ -1,5 +1,34 @@
 require 'reel'
 require 'websocket/driver'
+
+class ReelRouter
+  def initialize(routes)    
+    @routes = routes
+  end
+
+  def self.parse_post_params(env)
+    lst = env.body.to_s.split("\n").map(&:chomp).map{|s| s.split('&') }.flatten.map{|s| s.split('=') }.inject({}){|r, a| r.merge(a.first => a.last) }
+    p lst
+    lst
+  end
+
+  def default
+    [ 404, {'Content-Type' => 'text/plain'}, 'not found' ]
+  end
+
+  def call(env)
+    @routes.each do |route|
+      next unless env.method == route[:method]
+      match = env.url.match(route[:pattern])
+      # match = env['REQUEST_PATH'].match(route[:pattern])
+      if match
+        return route[:controller].call( env, match )
+      end
+    end
+    default
+  end
+end
+
 class Web < Reel::Server::HTTP
   include Celluloid
   include Celluloid::IO
@@ -40,8 +69,47 @@ class Web < Reel::Server::HTTP
     #   return render_index(connection)
     # end
 
-    info "404 Not Found: #{request.path}"
-    connection.respond :not_found, "Not found"
+    rout = ReelRouter.new(
+      [
+        # {
+        #   method: 'GET',
+        #   pattern: %r[^/api/v1/games/(?<id>[0-9A-Za-z-]+)],
+        #   controller: lambda {|env, match| game_params(match[:id]) }
+        # },
+        {
+          method: 'GET',
+          pattern: %r[^/api/v1/games/(?<id>[0-9A-Za-z-]+)],
+          controller: lambda {|env, match| game_params(match[:id]) }
+        },
+        {
+          method: 'GET',
+          pattern: %r[^/api/v1/games],
+          controller: lambda {|env, match| games_list }
+        },
+        {
+          method: 'POST',
+          pattern: %r[^/api/v1/games],
+          controller: lambda {|env, match| create_game(ReelRouter.parse_post_params(env)) }
+        }
+      ]
+    )
+
+    # info "404 Not Found: #{request.path}"
+    connection.respond *rout.call(request)
+    # connection.respond :not_found, "Not found"
+  end
+
+  def games_list
+    [:ok, 'test it']
+  end
+
+  def create_game(params)
+    p params
+    [:ok, 'test it2']
+  end
+
+  def game_params(id)
+    [:ok, 'test it']
   end
 
   def route_websocket(socket)
