73de1dfb0d244f2efac3754973b5510a2f2a1437
oleg <afa@cnwork.techranch.local>
2016-04-03 20:14:30 +0300

    rd

--- lib/state.rb
+++ lib/state.rb
@@ -124,7 +124,7 @@ class State
     end
     statements = Actor[:"statements_#{@guid}"]
     @step = 1
-    @total_steps = @setting[:max_steps] || 12
+    @total_steps = @setting[:max_steps] || 18
     @step_status = first_enum(STEP_STATUSES)
     statements.clean_current
 
