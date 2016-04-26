f3a290c05bd1803347ef758bc9c1d0822cd336de
oleg <afa@cnwork.techranch.local>
2016-04-26 17:50:43 +0300

    results

--- lib/players/player.rb
+++ lib/players/player.rb
@@ -160,12 +160,14 @@ class Player
   def send_game_results params = {}
     statements = Actor[:"statements_#{@game_uuid}"]
     players = Actor[:"players_#{@game_uuid}"]
-    stats = %w(s w o t).map(&:to_sym).inject({statements: []}) do |r, sym|
-      r[:statements] << statements.visible_for_buf(statements.in_stage(sym)).map{|s| {body: s.value, contribution: "%.03f" % s.contribution_for(@uuid)} }
+    stats = %w(s w o t).map(&:to_sym).inject({}) do |r, sym|
+      r.merge!(sym => {statements: []})
+      r[sym][:statements] += statements.visible_for_buf(statements.rebuild_visible_for(sym)).map{|s| {body: s.value, contribution: s.contribution_for(@uuid)} }
+      r
     end
     pls = players.players.sort{|a, b| a.uuid == b.uuid ? 0: a.uuid == @uuid ? -1 : a.uuid <=> b.uuid }
     cur = pls.shift
-    ps = [{cur.name => {pitcher_score: "%.03f" % cur.pitcher_score, catcher_score: "%.03f" % cur.catcher_score}}] + pls.map{|p| { p.uglified_name(:s) => {pitcher_score: "%.03f" % p.pitcher_score, catcher_score: "%.03f" % p.catcher_score} } }
+    ps = [{cur.name => {pitcher_score: cur.pitcher_score, catcher_score: cur.catcher_score}}] + pls.map{|p| { p.uglify_name(:s) => {pitcher_score: (p.pitcher_score), catcher_score: (p.catcher_score)} } }
     # { type: results, value: { data: { 's': { statements: [{ body: <str>, contribution: <float> }]}, 'w': { statements: [...] }, 'o': ..., 't': ... }, players: { real_name: { pitcher_score: <float>, catcher_score: <float> }, player_1: { ... }, player_3: { ... }...}}}
     msg = {type: 'results', value: { data: stats, players: ps } }
     publish_msg msg
