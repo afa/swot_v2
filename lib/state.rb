74ff826c97b9b8307b4da39332fd8d3ab705f405
oleg <afa@cnwork.techranch.local>
2016-04-27 12:00:44 +0300

    use settings max_steps

--- lib/state.rb
+++ lib/state.rb
@@ -125,7 +125,7 @@ class State
     end
     statements = Actor[:"statements_#{@guid}"]
     @step = 1
-    @total_steps = @setting[:max_steps] || 18
+    @total_steps = @setting[:max_steps] # || 18
     @step_status = first_enum(STEP_STATUSES)
     statements.clean_current
 
